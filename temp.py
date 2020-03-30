9e9c7f5ebeb6946d5a85ac66e6444d87d218c3af
Raj dubey <com.mailuser@gmail.com>
2020-03-30 16:27:38 +0530

    video save added

--- temp.py
+++ temp.py
@@ -4,11 +4,18 @@ cap=cv2.VideoCapture(0)
 wid=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+#for windows use : *'DIVX'
+#for linux use : *'XVID'
+
+writer=cv2.VideoWriter('initVideoOpenCV.mp4',cv2.VideoWriter_fourcc(*'XVID'),20,(wid,height))
 while True:
     ret,fr=cap.read()
     if cv2.waitKey(1) & 0xFF==27:
         break
     ff=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
     cv2.putText(fr,f"{datetime.datetime.now().strftime('%H:%M:%S')} {datetime.date.today()}",( 20,height-20),ff,1,(255,255,255))
+    writer.write(fr)
     cv2.imshow("Hello",fr)
 cv2.destroyAllWindows()
+cap.release()
+writer.release()
