3cdb486c9ccf7cfbf482ae91d756c8a339e41c3c
Raj dubey <com.mailuser@gmail.com>
2020-04-10 21:28:04 +0530

    reading from webcam

--- ip_webcam.py
+++ ip_webcam.py
@@ -2,25 +2,27 @@ import urllib.request
 import cv2
 import numpy as np
 import ssl
-
+import config
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
+def read_webcam():
 
-print("....")
-while True:
-    print("Starting")
-    imgResp = urllib.request.urlopen(url)
-    # print("gathered Image Response")
-    res_byte_arr=imgResp.read()
-    # print("gathered res byte array")
-    imgNp = np.array(bytearray(res_byte_arr), dtype=np.uint8)
-    # print("Converted to NP array")
-    img = cv2.imdecode(imgNp, -1)
-    # print("Decoded using imdecode")
-    cv2.imshow('temp',cv2.resize(img,(600,400)))
-    q = cv2.waitKey(1) & 0xFF
-    if q == 27:
-        break
+    try:
+        imgResp = urllib.request.urlopen(config.url_webcam)
+        # print("gathered Image Response")
+        res_byte_arr=imgResp.read()
+        # print("gathered res byte array")
+        imgNp = np.array(bytearray(res_byte_arr), dtype=np.uint8)
+        # print("Converted to NP array")
+        img = cv2.imdecode(imgNp, -1)
+        # print("Decoded using imdecode")
+        return img
+        # return the image
+    except Exception as e:
+        print("Exception occured ..",e)
+        print("Are you sure IP webcam app is running in your phone")
+        return None
 
-cv2.destroyAllWindows()
+
+read_webcam()
