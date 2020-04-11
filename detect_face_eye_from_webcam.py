8c3d8933f40481e022b37a7849a37c8568ec9bde
Raj dubey <com.mailuser@gmail.com>
2020-04-11 12:30:55 +0530

    ADDED detect eye face from webcam

---
+++ detect_face_eye_from_webcam.py
@@ -0,0 +1,41 @@
+import cv2
+import datetime
+import ip_webcam
+cap=cv2.VideoCapture(0)
+wid=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+eye_cascade=cv2.CascadeClassifier('/home/beast/python_Codes/openCV/haarcascades/haarcascade_eye.xml')
+face_cascade=cv2.CascadeClassifier('/home/beast/python_Codes/openCV/haarcascades/haarcascade_frontalface_default.xml')
+writer=cv2.VideoWriter('detect_face_eye.mp4',cv2.VideoWriter_fourcc(*'XVID'),20,(wid,height))
+
+def detect_eye(img):
+    raw_img=img.copy()
+    raw_img=cv2.cvtColor(raw_img,cv2.COLOR_BGR2GRAY)
+    eye_rects=eye_cascade.detectMultiScale(raw_img,scaleFactor=1.2,minNeighbors=5)
+    for (x,y,w,h) in eye_rects:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),5)
+    return 
+def detect_face(img):
+    raw_img=img.copy()
+    raw_img=cv2.cvtColor(raw_img,cv2.COLOR_BGR2GRAY)
+    face_rects=face_cascade.detectMultiScale(raw_img,scaleFactor=1.2,minNeighbors=5)
+    for (x,y,w,h) in face_rects:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),10)
+    return 
+
+
+while True:
+    fr=ip_webcam.read_webcam()
+
+    ff=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
+    detect_eye(fr)
+    detect_face(fr)
+    cv2.putText(fr,f"{datetime.datetime.now().strftime('%H:%M:%S')} {datetime.date.today()}",( 20,height-20),ff,1,(255,255,255))
+    cv2.imshow("Detect Face and Eye",fr)
+    writer.write(fr)
+
+    if cv2.waitKey(1) & 0xFF==27:
+        break
+cv2.destroyAllWindows()
+cap.release()
+writer.release()
